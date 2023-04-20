9f8f1e4778bc7cc7958c8ad6854ad44d5846f679
spyeic <lderic0415@gmail.com>
2023-04-20 01:55:29 -0700

    Add comment for Weight

--- Classes/cn/zzhxccelerator/weight/Weight.java
+++ Classes/cn/zzhxccelerator/weight/Weight.java
@@ -2,9 +2,17 @@ package cn.zzhxccelerator.weight;
 
 import java.lang.annotation.*;
 
+/**
+ * The weight of the face
+ * For methods with this annotation, the return value will be multiplied by the weight
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @Documented
 public @interface Weight {
+    /**
+     * The weight of the face
+     * @return the weight
+     */
     double value();
 }
